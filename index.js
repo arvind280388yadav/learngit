1b48d3f5bacc6d5f1321f65dfb45fa04a3fd072b
arvind280388yadav <yadav2mca@gmail.com>
2022-12-07 23:57:16 +0530

    add new content

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-<html>
-    <title>Arvind</title>
-    <body>
-        TEsting Git gitaksjdfkasjfdkasjdfkasjdk
-    </body>
-</html>
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render() {
+        return (
+            <button
+                className="square"
+                onClick={() => this.setState({ value: 'X' })}
+            >
+                {this.state.value}
+            </button>
+        );
+    }
+}
